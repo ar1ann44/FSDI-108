11088616b851407421e3ba87b97183a84e2a7c36
Ariana Osuna <osuna.jenifer@uabc.edu.mx>
2026-04-21 19:40:56 -0700

    dog age converter

---
+++ dog_age_converter.py
@@ -0,0 +1,6 @@
+print("-----dog age converter-----")
+dog_age = int(input("enter your dog's age in human years: "))
+
+dog_age_in_dog_years = dog_age * 7
+
+print(f"your dog is {dog_age_in_dog_years} in dog years")
